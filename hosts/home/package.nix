7e127c4396d44ab43dca88a132d4757f11a6d5b7
Jesse Malinosky <jesse@malinoskj2.dev>
2026-06-13 14:38:14 -0400

    fixes for cs2

--- hosts/home/package.nix
+++ hosts/home/package.nix
@@ -10,5 +10,7 @@
     pciutils
     lshw
     uutils-coreutils-noprefix
+    hyprpaper
+    waybar
   ];
 }
--- hosts/home/program.nix
+++ hosts/home/program.nix
@@ -5,6 +5,12 @@
   programs.ssh.askPassword = "";
   programs.ssh.startAgent = true;
   programs.zsh.enable = true;
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    localNetworkGameTransfers.openFirewall = true;
+  };
+  programs.gamemode.enable = true;
   programs.nix-ld.enable = true;
   programs.nix-ld.libraries = with pkgs; [
     libcap
