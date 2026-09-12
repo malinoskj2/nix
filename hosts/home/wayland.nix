e69106c5f68404465ac6c49c751b156c19b34846
Jesse Malinosky <jesse@malinoskj2.dev>
2026-09-12 14:59:34 -0400

    update kernel and nvidia driver

--- hosts/home/wayland.nix
+++ hosts/home/wayland.nix
@@ -30,6 +30,15 @@
       extraPackages32 = with pkgs.pkgsi686Linux; [ libva ];
     };
     nvidia = {
+      # Newer than 26.05's 595.71.05; hashes from nixos-unstable's production driver.
+      package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+        version = "595.99.02";
+        sha256_64bit = "sha256-6HR3lYv3YwcFSTJL1a1slI66btIQ5EAFs+/4SUD24ew=";
+        sha256_aarch64 = "sha256-CCqHZTN2KNOZ4yZp2rDcuRJp9pHfRw47k4m4dWnS/2w=";
+        openSha256 = "sha256-T36x/jx8yQ8l3LFp1rZIrTfcSwbGy8YSAvXOUSptpb4=";
+        settingsSha256 = "sha256-GYCcnxfKPrTCrsmd25sMyzfC5cqJQJx0c31haooyTYM=";
+        persistencedSha256 = "sha256-VyKtF/HdHPQrHHK6opSO69M72LmnGZtauuchj9uuje8=";
+      };
       modesetting.enable = true;
       open = true;
       nvidiaSettings = false;
