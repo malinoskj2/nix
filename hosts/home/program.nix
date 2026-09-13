d81d3c8ae89c9160859974284ad0d5858af12ab8
Jesse Malinosky <jesse@malinoskj2.dev>
2026-09-13 15:44:52 -0400

    add obs

--- hosts/home/program.nix
+++ hosts/home/program.nix
@@ -11,6 +11,15 @@
     localNetworkGameTransfers.openFirewall = true;
   };
   programs.gamemode.enable = true;
+  programs.obs-studio = {
+    enable = true;
+    # cudaSupport adds the driver runpath to obs-nvenc-test; without it OBS hides NVENC.
+    package = pkgs.obs-studio.override { cudaSupport = true; };
+    plugins = with pkgs.obs-studio-plugins; [
+      wlrobs
+      obs-pipewire-audio-capture
+    ];
+  };
   programs.nix-ld.enable = true;
   programs.nix-ld.libraries = with pkgs; [
     libcap
